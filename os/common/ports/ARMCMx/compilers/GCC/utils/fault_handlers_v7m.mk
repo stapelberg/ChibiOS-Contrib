f306b53a8b371b6cc86221050cbe646a1d252045
Michael Stapelberg <stapelberg@google.com>
2021-03-23 23:52:30 +0100

    remove hardfault handler in favor of chibios’s own

--- os/common/ports/ARMCMx/compilers/GCC/utils/fault_handlers_v7m.mk
+++ os/common/ports/ARMCMx/compilers/GCC/utils/fault_handlers_v7m.mk
@@ -1,4 +1,9 @@
-ALLINC		+= 	$(CHIBIOS_CONTRIB)/os/various/fault_handlers		\
+
+FAULTHANDLERSINC		= 	$(CHIBIOS_CONTRIB)/os/various/fault_handlers		\
 				$(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/utils/
-ALLCSRC 	+= 	$(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/utils/fault_handlers_v7m.c
-ALLXASMSRC 	+= 	$(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/utils/hardfault_handler_v7m.S
+FAULTHANDLERSSRC 	= 	$(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/utils/fault_handlers_v7m.c
+FAULTHANDLERSXASMSRC 	= 	$(CHIBIOS_CONTRIB)/os/common/ports/ARMCMx/compilers/GCC/utils/hardfault_handler_v7m.S
+
+ALLCSRC += $(FAULTHANDLERSSRC)
+ALLINC += $(FAULTHANDLERSINC)
+ALLXASMSRC += $(FAULTHANDLERSXASMSRC)
